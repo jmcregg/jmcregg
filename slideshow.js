7f4fa8d8aef6c225dc8c6bda5153a5db05b2fba7
jmcregg <125033184+jmcregg@users.noreply.github.com>
2023-03-06 20:09:14 +0100

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -1,5 +1,5 @@
 let slideIndex = [1,1];
-let slideId = ["mySlides1", "mySlides2"]
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8", "mySlides9", "mySlides10", "mySlides11", "mySlides12"]
 showSlides(1, 0);
 showSlides(1, 1);
 
