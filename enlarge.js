ef66dfb0826efa2ce6efae91c6d54af0a09697f1
jmcregg <125033184+jmcregg@users.noreply.github.com>
2023-04-13 18:02:43 +0200

    Create enlarge.js

---
+++ enlarge.js
@@ -0,0 +1,18 @@
+<script>
+
+function enlargeImg() {
+   var element = document.getElementById("myImg");
+   element.classList.toggle("largeimage");
+}
+
+function enlargeImg2() {
+   var element = document.getElementById("myImg2");
+   element.classList.toggle("largeimage");
+}
+
+function enlargeImg3() {
+   var element = document.getElementById("myImg3");
+   element.classList.toggle("largeimage");
+}
+
+</script>
