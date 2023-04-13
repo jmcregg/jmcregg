31be0b374cd477836c0b5bf8868e0ecd7f9d4bd6
jmcregg <125033184+jmcregg@users.noreply.github.com>
2023-04-13 21:53:13 +0200

    Update enlarge.js

--- enlarge.js
+++ enlarge.js
@@ -1,14 +1,14 @@
 function enlargeImg() {
-   var element = document.getElementById("myImg");
-   element.classList.toggle("largeimage");
+   var element = document.getElementById("myTable");
+   element.classList.toggle("largetable");
 }
 
 function enlargeImg2() {
-   var element2 = document.getElementById("myImg2");
+   var element2 = document.getElementById("myTable2");
    element2.classList.toggle("largeimage");
 }
 
 function enlargeImg3() {
-   var element3 = document.getElementById("myImg3");
+   var element3 = document.getElementById("myTable3");
    element3.classList.toggle("largeimage");
 }
