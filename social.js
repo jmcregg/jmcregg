93a46f8db9e29507ca0776bac1b8e3290183b70f
jmcregg <125033184+jmcregg@users.noreply.github.com>
2023-04-14 14:23:47 +0200

    Create social.js

---
+++ social.js
@@ -0,0 +1,14 @@
+function enlargeImg() {
+   var element = document.getElementById("TableOne");
+   element.classList.toggle("largetable");
+}
+
+function enlargeImg2() {
+   var element = document.getElementById("TableTwo");
+   element.classList.toggle("largetable");
+}
+
+function enlargeImg3() {
+   var element = document.getElementById("TableThree");
+   element.classList.toggle("largetable");
+}
