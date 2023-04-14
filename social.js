7afb2f43600aed65fa81eae46189c9916b0b1694
jmcregg <125033184+jmcregg@users.noreply.github.com>
2023-04-14 18:28:20 +0200

    Update social.js

--- social.js
+++ social.js
@@ -1,14 +1,94 @@
 function enlargeImg() {
    var element = document.getElementById("TableOne");
    element.classList.toggle("largetable");
+   
+   var x = document.getElementById("Row1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
 
 function enlargeImg2() {
    var element = document.getElementById("TableTwo");
    element.classList.toggle("largetable");
+   
+   var x = document.getElementById("Row2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
 
 function enlargeImg3() {
    var element = document.getElementById("TableThree");
    element.classList.toggle("largetable");
+   
+   var x = document.getElementById("Row3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
+
+function enlargeImg4() {
+   var element = document.getElementById("TableFour");
+   element.classList.toggle("largetable");
+   
+   var x = document.getElementById("Row4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function enlargeImg5() {
+   var element = document.getElementById("TableFive");
+   element.classList.toggle("largetable");
+   
+   var x = document.getElementById("Row5");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function enlargeImg6() {
+   var element = document.getElementById("TableSix");
+   element.classList.toggle("largetable");
+   
+   var x = document.getElementById("Row6");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function enlargeImg7() {
+   var element = document.getElementById("TableSeven");
+   element.classList.toggle("largetable");
+   
+   var x = document.getElementById("Row7");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function enlargeImg8() {
+   var element = document.getElementById("TableEight");
+   element.classList.toggle("largetable");
+   
+   var x = document.getElementById("Row8");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
