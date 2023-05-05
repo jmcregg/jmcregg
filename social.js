4fd3682b97e834e83dd34f04a14b239811e499cd
jmcregg <125033184+jmcregg@users.noreply.github.com>
2023-05-05 18:47:03 +0200

    Update social.js

--- social.js
+++ social.js
@@ -21,3 +21,51 @@ function enlargeImg2() {
     x.style.display = "none";
   }
 }
+
+function enlargeImg3() {
+   var element = document.getElementById("myImg3");
+   element.classList.toggle("imgTwo");
+ 
+   var x = document.getElementById("captionThree");
+  if (x.style.display === "none") {
+    x.style.display = "inline";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function enlargeImg4() {
+   var element = document.getElementById("myImg4");
+   element.classList.toggle("imgTwo");
+ 
+   var x = document.getElementById("captionFour");
+  if (x.style.display === "none") {
+    x.style.display = "inline";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function enlargeImg5() {
+   var element = document.getElementById("myImg5");
+   element.classList.toggle("imgTwo");
+ 
+   var x = document.getElementById("captionFive");
+  if (x.style.display === "none") {
+    x.style.display = "inline";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function enlargeImg6() {
+   var element = document.getElementById("myImg6");
+   element.classList.toggle("imgTwo");
+ 
+   var x = document.getElementById("captionSix");
+  if (x.style.display === "none") {
+    x.style.display = "inline";
+  } else {
+    x.style.display = "none";
+  }
+}
