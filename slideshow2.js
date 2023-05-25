7dd6c0878026c7696fbcb0dcf30948c4ec9c33da
Jared Cregg <125033184+jmcregg@users.noreply.github.com>
2023-05-25 21:33:14 +0200

    Update slideshow2.js

--- slideshow2.js
+++ slideshow2.js
@@ -1,19 +1,41 @@
 let slideIndex = 0;
+let slides = document.getElementsByClassName("mySlides");
+let dots = document.getElementsByClassName("dot");
+let timer; // Variable to hold the timer
+let slideDuration = 6000; // Duration for each slide in milliseconds
+
+// Add event listeners to dots
+for (let i = 0; i < dots.length; i++) {
+  dots[i].addEventListener("click", function() {
+    clearTimeout(timer); // Clear the timer
+    goToSlide(i);
+    startTimer(); // Start the timer for automatic slide transition
+  });
+}
+
 showSlides();
 
 function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dots[i].classList.remove("active"); // Remove active class from all dots
   }
+  slides[slideIndex].style.display = "block";
+  dots[slideIndex].classList.add("active"); // Add active class to the current dot
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 6000); // Change image every 2 seconds
+  startTimer(); // Start the timer for automatic slide transition
+}
+
+function startTimer() {
+  clearTimeout(timer); // Clear the timer
+  timer = setTimeout(showSlides, slideDuration);
+}
+
+function goToSlide(index) {
+  clearTimeout(timer); // Clear the timer
+  slideIndex = index;
+  showSlides();
 }
